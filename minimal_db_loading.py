e42105baccb6d2f328c062d30acd21f6af53cc92
Ole Mussmann <gitlab+account@ole.mn>
2024-09-23 10:47:27 +0200

    use more realistic examples

--- minimal_db_loading.py
+++ minimal_db_loading.py
@@ -2,5 +2,16 @@
 
 from spinedb_api import DatabaseMapping
 
-db = DatabaseMapping(f"sqlite:///databases/BB_data.sqlite")
-db.close()
+def bb_data():
+    db = DatabaseMapping(f"sqlite:///databases/BB_data.sqlite")
+    vals = list(db.get_items(item_type="parameter_value"))
+    db.close()
+
+def egypt():
+    db = DatabaseMapping(f"sqlite:///databases/egypt-national.sqlite")
+    vals = list(db.get_items(item_type="parameter_value"))
+    print(vals)
+    db.close()
+
+#bb_data()
+egypt()
