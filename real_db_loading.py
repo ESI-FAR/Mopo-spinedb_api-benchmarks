63f36e91c85e1ec104ba9f236154bc9fdcf3f306
Ole Mussmann <gitlab+account@ole.mn>
2024-09-25 15:08:12 +0200

    make script profilable

--- real_db_loading.py
+++ real_db_loading.py
@@ -6,6 +6,27 @@ from spinetoolbox.plotting import turn_node_to_xy_data
 DB_URL = "sqlite:///databases/BB_data.sqlite"
 #DB_URL = "sqlite:///databases/egypt-national.sqlite"
 
-db = DatabaseMapping(f"sqlite:///databases/BB_data.sqlite")
-vals = list(db.get_items(item_type="parameter_value"))
-db.close()
+#db = DatabaseMapping(DB_URL)
+#value = db.get_parameter_value_item(id=256351)
+#db.close()
+
+#print(type(value))
+
+#data = ast.literal_eval(value.__dict__["_mapped_item"]["value"].decode())
+
+#data_0 = data["data"][0][1][1]["data"]  # f00
+#data_1 = data["data"][1][1][1]["data"]  # avg
+#data_2 = data["data"][2][1][1]["data"]  # scen
+#data_3 = data["data"][3][1][1]["data"]  # median
+
+#print(data_2[1]["data"][0])
+
+#print(value["parsed_value"].values[2].values[-1])
+
+def get_datapoint(DB_URL):
+    with DatabaseMapping(DB_URL) as db:
+        value = db.get_parameter_value_item(id=256351)
+
+    print(value["parsed_value"].values[2].values[-1])
+
+get_datapoint("sqlite:///databases/BB_data.sqlite")
